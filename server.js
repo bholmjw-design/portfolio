f2ab2c2178c3df508dc5483ae76040065523a1bf
bholmjw-design <bholmjw@gmail.com>
2026-05-06 08:20:45 +0530

    Fix PORT for deployment

--- server.js
+++ server.js
@@ -21,5 +21,5 @@ io.on('connection', socket => {
   });
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log(`🎬 Running at http://localhost:${PORT}`));
